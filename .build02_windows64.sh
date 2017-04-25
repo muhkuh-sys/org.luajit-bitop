2c60abcd93debab439b60e9353b4ab808238ea52
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-04-25 17:09:36 +0200

    Use one buildscript for linux. Use a common script part for all builds.

--- .build02_windows64.sh
+++ .build02_windows64.sh
@@ -3,26 +3,16 @@ set -e
 
 PRJ_DIR=${PWD}
 
-# This is the path to the jonchki tool.
+# Set the build folder. All generated content will be below this path.
+BUILD_DIR="${PRJ_DIR}/build/windows64"
+
+# All CMAKE arguments defining the target system and the compiler for it.
+CMAKE_COMPILER="-DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres"
+
+# This is the absolute path to the jonchki tool.
 JONCHKI=${PRJ_DIR}/jonchki/local/jonchki.lua
+# All jonchki arguments to select the target system.
+JONCHKI_SYSTEM="--distribution-id windows --cpu-architecture x86_64"
 
-#-----------------------------------------------------------------------------
-#
-# Build the windows64 version.
-#
-rm -rf build_windows64
-mkdir build_windows64
-
-mkdir build_windows64/lua5.1
-mkdir build_windows64/lua5.1/build_requirements
-
-pushd build_windows64/lua5.1/build_requirements
-cmake -DBUILDCFG_ONLY_JONCHKI_CFG="ON" -DBUILDCFG_LUA_VERSION="5.1" -DCMAKE_INSTALL_PREFIX="" -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ${PRJ_DIR}
-make
-lua5.1 ${JONCHKI} --verbose info --syscfg ${PRJ_DIR}/jonchki/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/jonchkicfg.xml --distribution-id windows --cpu-architecture x86_64 --build-dependencies bitop/lua5.1-bitop-1.0.2.1.xml
-popd
-
-pushd build_windows64/lua5.1
-cmake -DBUILDCFG_LUA_USE_SYSTEM="OFF" -DBUILDCFG_LUA_VERSION="5.1" -DCMAKE_INSTALL_PREFIX="" -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ${PRJ_DIR}
-make
-popd
+# Build the project.
+. "${PRJ_DIR}/.build_common.sh"
---
+++ .build04_artefacts.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+set -e
+
+#-----------------------------------------------------------------------------
+#
+# Build all artefacts.
+#
+pushd build
+rm -rf org.luajit-lua5.1-bitop
+rm -rf org.luajit-lua5.2-bitop
+rm -rf lua5.1
+rm -rf lua5.2
+
+mkdir org.luajit-lua5.1-bitop
+mkdir org.luajit-lua5.2-bitop
+mkdir -p lua5.1/windows_x86
+mkdir -p lua5.1/windows_x86_64
+mkdir -p lua5.1/ubuntu_1404_x86
+mkdir -p lua5.1/ubuntu_1404_x86_64
+mkdir -p lua5.1/ubuntu_1604_x86
+mkdir -p lua5.1/ubuntu_1604_x86_64
+mkdir -p lua5.1/ubuntu_1610_x86
+mkdir -p lua5.1/ubuntu_1610_x86_64
+mkdir -p lua5.2/windows_x86
+mkdir -p lua5.2/windows_x86_64
+mkdir -p lua5.2/ubuntu_1404_x86
+mkdir -p lua5.2/ubuntu_1404_x86_64
+mkdir -p lua5.2/ubuntu_1604_x86
+mkdir -p lua5.2/ubuntu_1604_x86_64
+mkdir -p lua5.2/ubuntu_1610_x86
+mkdir -p lua5.2/ubuntu_1610_x86_64
+
+
+
+tar --extract --directory lua5.1/windows_x86 --file build_windows_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86 --file build_windows_x86_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/windows_x86_64 --file build_windows_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86_64 --file build_windows_x86_64_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1610_x86 --file build_ubuntu_1610_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1610_x86 --file build_ubuntu_1610_x86_lua5.2.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1610_x86_64 --file build_ubuntu_1610_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1610_x86_64 --file build_ubuntu_1610_x86_64_lua5.2.tar.gz --gzip
+
+popd
+
+
+pushd build/org.luajit-lua5.1-bitop
+cmake -DCMAKE_INSTALL_PREFIX="" ../../bitop/installer/lua5.1
+make
+make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/luajit/bitop/*/lua5.1-bitop-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/luajit/bitop/*/lua5.1-bitop-*.tar.xz
+popd
+
+pushd build/org.luajit-lua5.2-bitop
+cmake -DCMAKE_INSTALL_PREFIX="" ../../bitop/installer/lua5.2
+make
+make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/luajit/bitop/*/lua5.2-bitop-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/luajit/bitop/*/lua5.2-bitop-*.tar.xz
+popd
